e15ca6c1fa50c21cd06f9a0599aa2731fa931b4b
polytan02 <polytan02@mcgva.org>
2015-02-03 13:43:18 +0000

    Automatic field filling based on current_host

--- 5_apticron_jail2ban_email_reports.sh
+++ 5_apticron_jail2ban_email_reports.sh
@@ -26,57 +26,76 @@ fi
 
 # We check that the email_apti_s argument is given for apticron
 if [ -z $1 ] ;
-        then echo -e "\n" ; read -e -p "apticron sender's email address : " email_apti_s;
+        then echo -e "\napticron sender's email address :"
+        current_host=`cat /etc/yunohost/current_host`
+        email_default=admin@$current_host
+        echo -e "\n" ; read -e -p "(if you don't specify one, we will use $email_default) : " email_apti_s;
+        if [ -z $email_apti_s ] ;
+                then email_apti_s=$email_default;
+                if [ -z $email_apti_s ] ;
+                        then echo -e "\n$failed You must specifiy a sender's email address for apticron as first argument or when requested";
+                        echo -e "\nAborting before doing anything"
+                        exit;
+                fi;
+        fi;
         else email_apti_s=$1;
-fi; if [ -z $email_apti_s ] ;
-	then echo -e "\n$failed You must specifiy a sende's email address for apticron as first argument";
-        echo -e "\nAborting before doing anything"
-        exit;
 fi;
-
 
 # We check that the email_apti_r argument is given for apticron
 if [ -z $2 ] ;
-        then echo -e "\n" ; read -e -p "apticron reports receiving email address : " email_apti_r;
-        else email_apti_r=$2;
-fi; if [ -z $email_apti_r ] ;
-	then echo -e "\n$failed You must specifiy a receving email address for apticron reports as second argument";
-        echo -e "\nAborting before doing anything"
-        exit;
+        then echo -e "apticron reports receiving email address :"
+	echo -e "\n" ; read -e -p "(if you don't specify an email, we will use $email_apti_s) : " email_apti_r;
+	if [ -z $email_apti_r ] ;
+		then email_apti_r=$email_apti_s;
+		if [ -z $email_apti_r ] ;
+			then echo -e "\n$failed You must specifiy a receiver's email address for apticron as second argument or when requested";
+        		echo -e "\nAborting before doing anything"
+        		exit;
+		fi;
+	fi;
+       	else email_apti_r=$2;
 fi;
 
 # We check that the email_fail2ban argument is given for fail2ban
 if [ -z $3 ] ;
-        then echo -e "\n" ; read -e -p "fail2ban destination email : " email_fail2ban;
-        else email_apti_r=$3;
-fi; if [ -z $email_fail2ban ] ;
-	then echo -e "\n$failed You must specifiy a receving email address for fail2ban reports as third argument";
-        echo -e "\nAborting before doing anything"
-        exit;
+        then echo -e "fail2ban destination email address :"
+	echo -e "\n" ; read -e -p "(if you don't specify one, we will use $email_apti_s) : " email_fail2ban;
+	if [ -z $email_fail2ban ] ;
+		then email_fail2ban=$email_apti_s;
+		if [ -z $email_fail2ban ] ;
+			then echo -e "\n$failed You must specifiy a jail2ban email address as third argument or when requested";
+        		echo -e "\nAborting before doing anything"
+        		exit;
+		fi;
+	fi;
+       	else email_jail2ban=$3;
 fi;
 
 apti=/etc/apticron/apticron.conf
 cron=/etc/cron.d/apticron
 jail=/etc/fail2ban/jail.conf
 
-echo -e "$ok apticron Sender's email : $email_apti_s"
-echo -e "$ok apticron Receiver's email : $email_apti_r"
-echo -e "$ok fail2ban Receiver's email : $email_fail2ban"
-
 # We start by installing the right software
 echo -e "$ok Installation of apticron software"
 apt-get update
 apt-get install apticron
 
+echo -e "$ok apticron Sender's email : $email_apti_s"
+echo -e "$ok apticron Receiver's email : $email_apti_r"
+echo -e "$ok fail2ban Receiver's email : $email_fail2ban"
+
+
+
 # Then we configure apticron
-echo -e "$ok Configuring apticron to send emails to $email_apti_r "
+echo -e "$ok Configuring apticron to send emails from $email_apti_s "
 sed -i "s/EMAIL=\"root\"/EMAIL=\"$email_apti_s\"/g" $apti
 sed -i "s/# NOTIFY_NO_UPDATES=\"0\"/NOTIFY_NO_UPDATES=\"1\"/g" $apti
+echo -e "$ok Configuring apticron to receive emails to $email_apti_r "
 sed -i "s/# CUSTOM_FROM=\"\"/CUSTOM_FROM=\"$email_apti_r\"/g" $apti
 
 # We adjust the cron
 echo -e "$ok Adjustment of $cron"
-sed -i "s/8 \*/8 4/g" $cron
+sed -i "s/\* \* \* \*/4 \* \* \*/g" $cron
 
 # We edit jail.conf
 echo -e "$ok Configuring fail2ban to send emails to $email_fail2ban"
